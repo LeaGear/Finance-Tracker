0fc3ff000a19c098c5076025cde4b96686c241eb
LeaGear <franklinuaua@gmail.com>
2026-01-31 12:50:08 +0200

    Mini refactoring

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
-from operations import *
+import operations
 
-choice = 10
 
-while choice != 6:
-    print("Main Menu:\n"
-          "1 - Add operation\n"
-          "2 - Show balance\n"
-          "3 - Show history\n"
-          "4 - Show statistics\n"
-          "5 - Save operation\n"
-          "6 - Exit")
-    choice = int(input("What do you want: "))
-    if choice == 1:
-        add_oper()
-    elif choice == 2:
-        show_balance()
-    elif choice == 3:
-        show_history()
-    elif choice == 4:
-        show_stat()
-    elif choice == 5:
-        save_oper()
-    elif choice == 6:
-        print("Exiting...")
-        break
+def main():
+    while True:
+        print("Main Menu:\n"
+              "1 - Add operation\n"
+              "2 - Show balance\n"
+              "3 - Show history\n"
+              "4 - Show statistics\n"
+              "5 - Save operation\n"
+              "6 - Exit")
+        choice = int(input("What do you want: "))
+        if choice == 1:
+            operations.add_oper()
+        elif choice == 2:
+            operations.show_balance()
+        elif choice == 3:
+            operations.show_history()
+        elif choice == 4:
+            operations.show_stat()
+        elif choice == 5:
+            operations.save_oper()
+        elif choice == 6:
+            print("Exiting...")
+            break
+
+main()
--- operations.py
+++ operations.py
@@ -1,14 +1,13 @@
 from obj_constructor import constructor
-from storage import load, save
-from stats import *
-
+import storage
+import stats
 
 def add_oper(): #Function receive data from user and create income or expense object
     global data
     data = constructor()
 
 def show_balance(): #Sum up income, expense and calculating balance
-    all_data = load()
+    all_data = storage.load()
     income = 0
     expense = 0
     for misc in all_data:
@@ -22,8 +21,7 @@ def show_balance(): #Sum up income, expense and calculating balance
 
 
 def show_history(): #Showes tha latest 5 added object
-    all_data = load()
-    story = []
+    all_data = storage.load()
     story = all_data[-5:]
     for i in story: print(i, "\n")
 
@@ -34,16 +32,16 @@ def show_stat(): #Showes stats
               "1 - Income\n"
               "2 - Expense\nEnter: "))
         if tm == 1:
-            income_stats()
+            stats.income_stats()
             break
         elif tm == 2:
-            expense_stat()
+            stats.expense_stat()
             break
         else:
             print("Wrong input. Enter 1 or 2!")
 
 def save_oper(): #Function for saving list in JSON file
-    all_data = load() #Load JSON file
+    all_data = storage.load() #Load JSON file
     data["id"] = len(all_data) #Added an ID to the entry
     all_data.append(data) #Added object to list with all data
-    save(all_data) #Saving in JSON file
+    storage.save(all_data) #Saving in JSON file
